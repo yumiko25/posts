c0f132d9ff18d170d8c844fae8537356371c452d
Ubuntu <ubuntu@ip-172-31-37-0.ap-northeast-1.compute.internal>
2020-08-17 04:20:24 +0000

    log out

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -8,12 +8,14 @@
 
                 <div class="collapse navbar-collapse" id="nav-bar">
                     <ul class="navbar-nav mr-auto"></ul>
-                     <ul class="nav navbar-nav navbar-right">
+                     <ul class="navbar-nav navbar-right">
                          @if (Auth::check())
                              <li class="nav-item dropdown">
+                                  <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">{{ Auth::user()->name }}</a>
                                   <ul class="dropdown-menu dropdown-menu-right">
-                                      {{-- ログアウトへのリンク --}}
-                           　 <li class="dropdown-item">{!! link_to_route('logout.get', 'Logout') !!}</li>
+                                       <li class="dropdown-divider"></li>
+                                       {{-- ログアウトへのリンク --}}
+                           　           <li class="dropdown-item">{!! link_to_route('logout.get', 'Logout') !!}</li>
                            　      </ul>
                            　 </li>
                                  
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,39 @@
 @extends('layouts.app')
 
 @section('content')
+ @if (Auth::check())
+        <div class="row">
+        {{-- 投稿一覧 --}}
+            @include('posts.posts')
+        </div>
+@else
     <header style="background-color:skyblue">保育・教育業界から次のステップへ</br>～保育・教育業界から異業種への転経験投稿サイト～
            <div class = "text center">
             <nav id="global_navi">
                 <ul>
                     <li class="current"><a href="posts.welcome">ホーム</a></li>
                     <li><a href="posts.index">投稿一覧</a></li>
-                    <li><a href="">ログイン</a></li>
+                    <li><a href="login">ログイン</a></li>
                     <li><a href="signup.get">会員登録</a></li>
                 </ul>
             </nav>
             </div>
     </header>
+    <div class="row">
+            <aside class="col-sm-4">
+                <h3>ホームページの概要</h3>
+            </aside>
+            <div class="col-sm-8">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">あき</h3>
+                    </div>
+                    <div class="card-body">
+                       ４年目保育士
+                    </div>
+                </div>
+            </div>
+    </div>
 <div class="container-fluid">
   <div class="row">
     <div class="col-sm-2" style="background-color:red;">Red</div>
@@ -20,4 +41,5 @@
     <div class="col-sm-2" style="background-color:yellow;">Yellow</div>
   </div>
 </div>
+@endif
 @endsection
