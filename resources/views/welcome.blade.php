437f7ec13876f80e6ccbadcdc3bec05ba2d052a3
Ubuntu <ubuntu@ip-172-31-37-0.ap-northeast-1.compute.internal>
2020-08-21 15:51:33 +0000

    header color

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,20 +7,9 @@
             @include('posts.index')
         </div>
 @else
-    <header style="background-color:skyblue">
-        <h1>保育・教育業界から次のステップへ</br>～保育・教育業界から異業種への転経験投稿サイト～</h1>
-           <div class = "row">
-                <div class="gnavi__inner">
-                <nav id="global_navi">
-                    <ul>
-                        <li class="current"><a href="/">ホーム</a></li>
-                        <li><a href="home">投稿一覧</a></li>
-                        <li><a href="login">ログイン</a></li>
-                        <li><a href="signup">会員登録</a></li>
-                    </ul>
-                 </div> 
-                 </nav>
-            </div>
+    <header style = "background-color:#FFFF66">
+        <h1>保育・教育業界から次のステップへ</br>～保育・教育業界から異業種への転職経験投稿サイト～</h1>
+        
     </header>
     <div class="row">
             <aside class="col-sm-4">
@@ -37,12 +26,6 @@
                 </div>
             </div>
     </div>
-<div class="container-fluid">
-  <div class="row">
-    <div class="col-sm-2" style="background-color:red;">Red</div>
-    <div class="col-sm-8" style="background-color:blue;">Blue</div>
-    <div class="col-sm-2" style="background-color:yellow;">Yellow</div>
-  </div>
-</div>
+
 @endif
 @endsection
