b6f1f1555d3ae621670b0182d983ef34303b9181
Ubuntu <ubuntu@ip-172-31-37-0.ap-northeast-1.compute.internal>
2020-08-27 05:16:40 +0000

    form label

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,33 +1,40 @@
 @extends('layouts.app')
 
 @section('content')
-
-<h1>投稿の作成</h1>
-
+<div class="text-center">
+    <h1>投稿の作成</h1>
+ </div>
     <div class="row">
-        <div class="col-6">
+        <!--<div class="col-10 m-auto">-->
+        <div class="col-sm-9 offset-sm-3">
             {!! Form::model($post, ['route' => 'posts.store']) !!}
 
                 <div class="form-group">
                     {!! Form::label('title', 'タイトル:') !!}
                     {!! Form::text('title', null, ['class' => 'form-control']) !!}
-                    
+                </div>  
+                <div class="form-group">
                     {!! Form::label('name', 'ニックネーム:') !!}
                     {!! Form::text('name', null, ['class' => 'form-control']) !!}
-                    
+                </div>
+                <div class="form-group">   
                     {!! Form::label('work', '前職→現職:') !!}
                     {!! Form::text('work', null, ['class' => 'form-control']) !!}
-                    
-                    {!! Form::label('good_thing_content', '転職をして良かったこと:') !!}
-                    {!! Form::text('good_thing_content', null, ['class' => 'form-control']) !!}
-                    
-                    {!! Form::label('bad_thing_content', '転職をして気になったこと:') !!}
-                    {!! Form::text('bad_thing_content', null, ['class' => 'form-control']) !!}
-                    
-                    {!! Form::label('advice', 'アドバイス:') !!}
-                    {!! Form::text('advice', null, ['class' => 'form-control']) !!}
                 </div>
-
+                <div class="form-group">    
+                    {!! Form::label('good_thing_content', '転職をして良かったこと:') !!}
+                    {!! Form::textarea('good_thing_content', null, ['class' => 'form-control', 'size' => '15x3']) !!}
+                   
+                </div>
+                <div class="form-group">    
+                    {!! Form::label('bad_thing_content', '転職をして気になったこと:') !!}
+                    {!! Form::text('bad_thing_content', null, ['class' => 'form-control', 'size' => '15x3']) !!}
+                </div>
+                <div class="form-group">    
+                    {!! Form::label('advice', 'アドバイス:') !!}
+                    {!! Form::text('advice', null, ['class' => 'form-control', 'size' => '15x3']) !!}
+                </div>
+               
                 {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
 
             {!! Form::close() !!}
