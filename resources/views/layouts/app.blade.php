0c56be4b08990127acbf6598f63e663e1b244821
Ubuntu <ubuntu@ip-172-31-37-0.ap-northeast-1.compute.internal>
2020-08-28 07:53:49 +0000

    complete

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,9 +4,10 @@
         <meta charset="utf-8">
         <title>保育・教育業界から次のステップへ</title>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <meta name="description" content="保育・教育業界から異業種の仕事に転職した経験談を投稿する投稿サイトです。">
+        <meta name="description" content="保育・教育業界から異業種の仕事に転職した経験談を投稿する投稿サイトです。これから転職を目指している方に向けて、保育士や幼稚園教諭・教師から異業種に転職した転職方法や経験談・アドバイスなどを詳しく教えて下さい。">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
         <link href="{{asset('css/styles.css')}}" rel="stylesheet">
+        <link rel="shortcut icon" href="{{ asset('/img/hoikushi-hiyoko.png') }}">
     </head>
     
     <body>
